ad479c047bdf00edee1eb4b2efa8727eb016dda3
AHamon47 <ahamon47@gmail.com>
2022-12-21 14:47:27 +0100

    integration of channel class

--- M2e/src/app/core/models/channel.ts
+++ M2e/src/app/core/models/channel.ts
@@ -1,2 +1,10 @@
-export class Channel {
+import { ChannelI } from "../interfaces/channel-i";
+import { User } from "./user";
+
+export class Channel implements ChannelI {
+  id!: number;
+  user!: User;
+  title!: string;
+  description!: string;
+  has_news!: boolean;
 }
